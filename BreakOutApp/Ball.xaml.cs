9cef36e68de4a5355735604939b0fc8fe5f0ddd8
Jere Valtanen <jerevaltanen@msn.com>
2016-03-22 10:32:32 +0200

    Initial commit

---
+++ BreakOutApp/Ball.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BreakOutApp
+{
+    public sealed partial class Ball : UserControl
+    {
+        // location
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+        // speed
+        public double SpeedX { get; set; }
+        public double SpeedY { get; set; }
+        public Ball()
+        {
+            this.InitializeComponent();
+
+            Width = 15;
+            Height = 15;
+            SpeedX = -6;
+            SpeedY = -6;
+        }
+
+        // return balls rect
+        public Rect GetRect()
+        {
+            return new Rect(LocationX, LocationY, Width, Height);
+        }
+
+        // move ball
+
+        public void Move()
+        {
+            // change in x
+            LocationX = LocationX + SpeedX;
+            if (LocationX < 0)
+            {
+                LocationX = 0;
+                SpeedX *= -1;
+            }
+            else if (LocationX + Width > MainPage.CanvasWidth) // right
+            {
+                LocationX = MainPage.CanvasWidth - Width;
+                SpeedX *= -1;
+            }
+            // Change in y
+            LocationY = LocationY + SpeedY;
+            if (LocationY < 0) // up
+            {
+                LocationY = 0;
+                SpeedY *= -1;
+            }
+            // Move ball in canvas
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY); 
+        }
+
+        // ball speed, hit percent -0.5 <-> 0.5
+        public void SetSpeed(double hitPercent)
+        {
+            SpeedX = hitPercent * 10; // -5 <-> 5
+            SpeedY *= -1;
+        }
+    }
+}
---
+++ BreakOutApp/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BreakOutApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Canvas size
+        public static double CanvasWidth;
+        public static double CanvasHeight;
+
+        // game
+        private Game game;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+
+
+            // used in ball and paddle
+            CanvasWidth = MyCanvas.Width;
+            CanvasHeight = MyCanvas.Height;
+
+            // Create a new game
+            game = new Game(MyCanvas);
+            game.StartGame();
+        }
+
+        private void MyCanvas_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            PointerPoint point = e.GetCurrentPoint(this);
+            // move paddle...
+            game.paddle.Move(point.Position.X);
+        }
+    }
+}
